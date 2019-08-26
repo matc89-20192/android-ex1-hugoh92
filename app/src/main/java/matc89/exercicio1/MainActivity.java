d7983e707f9194700e34e09db5b4847a53bf611e
hugoh92 <hugoholiveira45@gmail.com>
2019-08-25 22:45:50 -0300

    segundo commit

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,6 +2,12 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCumprimentar = (Button) findViewById(R.id.btnCumprimentar);
+        final EditText editNome = (EditText) findViewById(R.id.editNome);
+        final TextView labelMensagem = (TextView) findViewById(R.id.labelMensagem);
+
+        btnCumprimentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                labelMensagem.setText("Alô, "+(editNome.getText()+"!"));
+
+            }
+
+
+
+        });
     }
 }
+
